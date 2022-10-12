7346d93968d4dcb35a4135e38e50efd60c9ee29b
hotpotkiller11 <zzq2018@bupt.edu.cn>
2022-10-11 23:55:07 -0400

    memcache3/5 methods, not linked with front end

--- Memcache/config.py
+++ Memcache/config.py
@@ -1,7 +1,6 @@
+
 class Config():
-'''
-Configuration information
-'''
+
     MEMCACHE_FOLDER = '.cacheImage' #floder for memcache
     IMAGE_FORMAT = ['.jpg', '.jpeg', '.png', '.tiff', '.gif', '.tif', '.bmp',
                     '.raw', '.cr2', '.nef', '.orf', '.sr2', '.psd', '.xcf', '.ai', 'cdr']
